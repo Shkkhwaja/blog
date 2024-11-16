2e16d6a182a6e7836c99c4458932205ba19b8470
Khwaja Shaikh <109279460+Shkkhwaja@users.noreply.github.com>
2024-11-16 17:37:01 +0530

    first commit

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Blog from "@/Components/Blog/Blog";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Blog />
+    </div>
+  );
+}
